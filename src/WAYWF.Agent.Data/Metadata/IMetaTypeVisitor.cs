4148d299324be49a72efbfc5c26a9cbe06f4b58f
brian-reichle <18721383+brian-reichle@users.noreply.github.com>
2026-08-04 20:58:22 +1000

    Fix SA1402.

--- src/WAYWF.Agent.Data/Metadata/IMetaTypeVisitor.cs
+++ src/WAYWF.Agent.Data/Metadata/IMetaTypeVisitor.cs
@@ -14,18 +14,4 @@ namespace WAYWF.Agent.Data
 		void VisitUnresolved(MetaUnresolvedType metaType);
 		void VisitVar(MetaVarType metaType);
 	}
-
-	public interface IMetaTypeVisitor<TArg, TResult>
-	{
-		TResult VisitArray(MetaArrayType metaType, TArg arg);
-		TResult VisitEnum(MetaEnumType metaType, TArg arg);
-		TResult VisitGCHandle(MetaGCHandleType metaType, TArg arg);
-		TResult VisitGen(MetaGenType metaType, TArg arg);
-		TResult VisitKnownType(MetaKnownType metaType, TArg arg);
-		TResult VisitNullable(MetaNullableType metaType, TArg arg);
-		TResult VisitPointer(MetaPointerType metaType, TArg arg);
-		TResult VisitSimpleResolved(MetaSimpleResolvedType metaType, TArg arg);
-		TResult VisitUnresolved(MetaUnresolvedType metaType, TArg arg);
-		TResult VisitVar(MetaVarType metaType, TArg arg);
-	}
 }
