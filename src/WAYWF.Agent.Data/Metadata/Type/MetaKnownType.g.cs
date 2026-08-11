575d4d328b97b1c46b39637e718621f767848b96
brian-reichle <18721383+brian-reichle@users.noreply.github.com>
2026-08-11 17:43:27 +1000

    Most projects don't actually need "unsafe" code.

--- src/WAYWF.Agent.Data/Metadata/Type/MetaKnownType.g.cs
+++ src/WAYWF.Agent.Data/Metadata/Type/MetaKnownType.g.cs
@@ -1,27 +1,28 @@
 // Copyright (c) Brian Reichle.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 
 namespace WAYWF.Agent.Data
 {
-	public unsafe partial class MetaKnownType
+	public partial class MetaKnownType
 	{
-		public static readonly MetaKnownType Boolean = new MetaKnownType(MetaKnownTypeCode.Boolean, "System.Boolean", sizeof(bool));
-		public static readonly MetaKnownType Char = new MetaKnownType(MetaKnownTypeCode.Char, "System.Char", sizeof(char));
-		public static readonly MetaKnownType SByte = new MetaKnownType(MetaKnownTypeCode.SByte, "System.SByte", sizeof(sbyte));
-		public static readonly MetaKnownType Int16 = new MetaKnownType(MetaKnownTypeCode.Int16, "System.Int16", sizeof(short));
-		public static readonly MetaKnownType Int32 = new MetaKnownType(MetaKnownTypeCode.Int32, "System.Int32", sizeof(int));
-		public static readonly MetaKnownType Int64 = new MetaKnownType(MetaKnownTypeCode.Int64, "System.Int64", sizeof(long));
-		public static readonly MetaKnownType IntPtr = new MetaKnownType(MetaKnownTypeCode.IntPtr, "System.IntPtr", sizeof(IntPtr));
-		public static readonly MetaKnownType Byte = new MetaKnownType(MetaKnownTypeCode.Byte, "System.Byte", sizeof(byte));
-		public static readonly MetaKnownType UInt16 = new MetaKnownType(MetaKnownTypeCode.UInt16, "System.UInt16", sizeof(ushort));
-		public static readonly MetaKnownType UInt32 = new MetaKnownType(MetaKnownTypeCode.UInt32, "System.UInt32", sizeof(uint));
-		public static readonly MetaKnownType UInt64 = new MetaKnownType(MetaKnownTypeCode.UInt64, "System.UInt64", sizeof(ulong));
-		public static readonly MetaKnownType UIntPtr = new MetaKnownType(MetaKnownTypeCode.UIntPtr, "System.UIntPtr", sizeof(UIntPtr));
-		public static readonly MetaKnownType Single = new MetaKnownType(MetaKnownTypeCode.Single, "System.Single", sizeof(float));
-		public static readonly MetaKnownType Double = new MetaKnownType(MetaKnownTypeCode.Double, "System.Double", sizeof(double));
-		public static readonly MetaKnownType Decimal = new MetaKnownType(MetaKnownTypeCode.Decimal, "System.Decimal", sizeof(decimal));
-		public static readonly MetaKnownType Guid = new MetaKnownType(MetaKnownTypeCode.Guid, "System.Guid", sizeof(Guid));
+		public static readonly MetaKnownType Boolean = new MetaKnownType(MetaKnownTypeCode.Boolean, "System.Boolean", Unsafe.SizeOf<bool>());
+		public static readonly MetaKnownType Char = new MetaKnownType(MetaKnownTypeCode.Char, "System.Char", Unsafe.SizeOf<char>());
+		public static readonly MetaKnownType SByte = new MetaKnownType(MetaKnownTypeCode.SByte, "System.SByte", Unsafe.SizeOf<sbyte>());
+		public static readonly MetaKnownType Int16 = new MetaKnownType(MetaKnownTypeCode.Int16, "System.Int16", Unsafe.SizeOf<short>());
+		public static readonly MetaKnownType Int32 = new MetaKnownType(MetaKnownTypeCode.Int32, "System.Int32", Unsafe.SizeOf<int>());
+		public static readonly MetaKnownType Int64 = new MetaKnownType(MetaKnownTypeCode.Int64, "System.Int64", Unsafe.SizeOf<long>());
+		public static readonly MetaKnownType IntPtr = new MetaKnownType(MetaKnownTypeCode.IntPtr, "System.IntPtr", Unsafe.SizeOf<IntPtr>());
+		public static readonly MetaKnownType Byte = new MetaKnownType(MetaKnownTypeCode.Byte, "System.Byte", Unsafe.SizeOf<byte>());
+		public static readonly MetaKnownType UInt16 = new MetaKnownType(MetaKnownTypeCode.UInt16, "System.UInt16", Unsafe.SizeOf<ushort>());
+		public static readonly MetaKnownType UInt32 = new MetaKnownType(MetaKnownTypeCode.UInt32, "System.UInt32", Unsafe.SizeOf<uint>());
+		public static readonly MetaKnownType UInt64 = new MetaKnownType(MetaKnownTypeCode.UInt64, "System.UInt64", Unsafe.SizeOf<ulong>());
+		public static readonly MetaKnownType UIntPtr = new MetaKnownType(MetaKnownTypeCode.UIntPtr, "System.UIntPtr", Unsafe.SizeOf<UIntPtr>());
+		public static readonly MetaKnownType Single = new MetaKnownType(MetaKnownTypeCode.Single, "System.Single", Unsafe.SizeOf<float>());
+		public static readonly MetaKnownType Double = new MetaKnownType(MetaKnownTypeCode.Double, "System.Double", Unsafe.SizeOf<double>());
+		public static readonly MetaKnownType Decimal = new MetaKnownType(MetaKnownTypeCode.Decimal, "System.Decimal", Unsafe.SizeOf<decimal>());
+		public static readonly MetaKnownType Guid = new MetaKnownType(MetaKnownTypeCode.Guid, "System.Guid", Unsafe.SizeOf<Guid>());
 		public static readonly MetaKnownType Void = new MetaKnownType(MetaKnownTypeCode.Void, "System.Void", 0);
 		public static readonly MetaKnownType Object = new MetaKnownType(MetaKnownTypeCode.Object, "System.Object", 0);
 		public static readonly MetaKnownType TypedReference = new MetaKnownType(MetaKnownTypeCode.TypedReference, "System.TypedReference", 0);
